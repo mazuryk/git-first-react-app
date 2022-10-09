19698588c46203d00a35ce44dbb726e815bb588f
mazuryk <olexandrmazuryk@gmail.com>
2022-10-09 17:59:01 +0300

    Lesson 20. NavLink

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styleCss from './Nav.module.css';
 
 const Navbar = () => {
   return (
     <nav className={styleCss.nav}>
       <div className={styleCss.item}>
-        <a href='/profile'>Profile</a>
+        <NavLink to='/profile' className={(navData) => (navData.isActive? styleCss.activeLink: styleCss.item)}>Profile</NavLink>
       </div>
-      <div className={`${styleCss.item} ${styleCss.active}`}>
-        <a href='/dialogs'>Message</a>
-      </div>
-      <div className={styleCss.item}>
-        <a href='/news'>News</a>
+      <div className={`${styleCss.item} ${styleCss.activeLink}`}>
+        <NavLink to='/dialogs' className={(navData) => (navData.isActive? styleCss.activeLink: styleCss.item)}>Message</NavLink>
       </div>
       <div className={styleCss.item}>
-        <a href='/music'>Music</a>
+        <NavLink to='/news' className={(navData) => (navData.isActive? styleCss.activeLink: styleCss.item)}>News</NavLink>
       </div>
       <div className={styleCss.item}>
-        <a href='/settings'>Settings</a>
+        <NavLink to='/music' className={(navData) => (navData.isActive? styleCss.activeLink: styleCss.item)}>Music</NavLink>
+      </div>
+      <div className={styleCss.item}>
+        <NavLink to='/settings' className={(navData) => (navData.isActive? styleCss.activeLink: styleCss.item)}>Settings</NavLink>
       </div>
     </nav>
   );
