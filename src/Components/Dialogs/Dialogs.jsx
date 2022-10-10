dc8397265d1f80231888dd51d64008a0908cd9e1
mazuryk <olexandrmazuryk@gmail.com>
2022-10-10 23:12:50 +0300

    lesson 21. Dialogs page

--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import styleCss from "./Dialogs.module.css"
+import styleCss from "./Dialogs.module.css";
 
-
-
-const Dialogs = (props) =>{
-    return(
-        <div>
-            Dialogs
-        </div>
-    )
-}
+const Dialogs = (props) => {
+  return (
+    <div className={styleCss.dialogs}>
+      <div className={styleCss.dialog_with_user}>
+        <div className={styleCss.item_dialog+' '+styleCss.active}>Sashko</div>
+        <div className={styleCss.item_dialog}>Marichka</div>
+        <div className={styleCss.item_dialog}>Ustym</div>
+        <div className={styleCss.item_dialog}>tato KiKi</div>
+        <div className={styleCss.item_dialog}>mama Katya</div>
+        <div className={styleCss.item_dialog}>babusya Nadya</div>
+        <div className={styleCss.item_dialog}>did Ivan</div>
+        <div className={styleCss.item_dialog}>babusya Natasha</div>
+      </div>
+      <div className={styleCss.messages}>
+        <div className={styleCss.messages}>Hi</div>
+        <div className={styleCss.messages}>Hello</div>
+        <div className={styleCss.messages}>How is your studying?</div>
+        <div className={styleCss.messages}>It is normal</div>
+      </div>
+    </div>
+  );
+};
 
 export default Dialogs;
